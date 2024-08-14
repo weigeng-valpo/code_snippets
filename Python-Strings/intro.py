075b609ac913bf8ef92fc514e3d0beec91ce04d6
David Geng <david.geng@valpo.edu>
2024-08-14 00:07:36 -0500

    update_08132024_wg

--- Python-Strings/intro.py
+++ Python-Strings/intro.py
@@ -1,5 +1,19 @@
+message = 'Hello, World!'
+print(message[10]) 
+print(message[0:5]) 
+print(message[6:]) 
+# this is called slicing
+
+print(message[-1]) 
+print(message[-5:-2]) 
+
+print(len(message))
 
 greeting = 'Hello'
 name = 'Michael'
 
+message = f'{greeting}, {name}'
+print(message)
+
 print(help(str.lower))
+print(help(str.upper)) 
