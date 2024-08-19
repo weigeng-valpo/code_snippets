574dc84165480e282989588fd5c5a5261f1eda28
weigeng-valpo <david.geng@valpo.edu>
2024-08-18 21:21:54 -0500

    update string

--- Python-Strings/intro.py
+++ Python-Strings/intro.py
@@ -1,5 +1,20 @@
+message = 'Hello, World!'
+print(message[10]) 
+print(message[0:5]) 
+print(message[6:]) 
+# this is called slicing
+
+print(message[-1]) 
+print(message[-5:-2]) 
+# doing backward slicing
+
+print(len(message))
 
 greeting = 'Hello'
 name = 'Michael'
 
+message = f'{greeting}, {name}'
+print(message)
+
 print(help(str.lower))
+print(help(str.upper)) 
